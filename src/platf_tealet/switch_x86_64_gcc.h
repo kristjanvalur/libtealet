87fe0ae9a88f81c3fc4eee1b0e2c9194c95b8a85
Kristján Valur Jónsson <sweskman@gmail.com>
2019-12-12 15:02:29 +0000

    Clean up gcc x86-64 switch.  Use frame pointer. pass correct rsp second time.

--- src/platf_tealet/switch_x86_64_gcc.h
+++ src/platf_tealet/switch_x86_64_gcc.h
@@ -13,14 +13,15 @@
  * floating point functions is therefore dangerous.
  */
 
-#define PRESERVE "rbx","r12", "r13", "r14", "r15","rbp"
+#define PRESERVE "rbx", "r12", "r13", "r14", "r15"
 
 /* Need the optimization level to avoid storing 'stack_pointer'
+ * and other locals
  * on the stack which would cause the wrong value to be sent to 
  * the second callback call (it would be read from stack).
- * Also, avoid a frame being put in rbp, allowing us to push it.
+ * Also, ensure a frame pointer is made, pushing rbp.
  */
-__attribute__((optimize("O", "omit-frame-pointer")))
+__attribute__((optimize("O", "-no-omit-frame-pointer")))
 void *tealet_slp_switch(tealet_save_restore_t save_restore_cb,
                         void *context)
 {
@@ -34,10 +35,6 @@ void *tealet_slp_switch(tealet_save_restore_t save_restore_cb,
 	/* set stack pointer from sp using assembly */
 	__asm__ ("movq %[result], %%rsp" :: [result] "r" (stack_pointer));
 
-	/* read it back, so that the value isnt read from a stack var
-	 * for the next call. For -O1, this is optimized away.
-	 */
-	__asm__ ("movq %%rsp, %[result]" : [result] "=r" (stack_pointer));
 	stack_pointer = save_restore_cb(context, stack_pointer);
 	/* restore non-volatile registers from stack */
 	return stack_pointer;
