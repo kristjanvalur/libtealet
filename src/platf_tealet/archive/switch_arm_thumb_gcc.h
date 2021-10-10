55a7baf85011c928e17ea22a0aa247e9d326c2fe
Kristján Valur Jónsson <sweskman@gmail.com>
2021-10-10 17:17:10 +0000

    remove legacy platform files, now part of stackman.

--- src/platf_tealet/archive/switch_arm_thumb_gcc.h
+++
@@ -1,9 +0,0 @@
-
-/* Use the generic support for an external assembly language slp_switch
- * function.
- */
-#define EXTERNAL_ASM switch_arm_thumb_gas.s
-#ifdef __ASSEMBLER__
-#include "switch_arm_thumb_gas.s"
-#endif
-
--- src/platf_tealet/switch_x64_msvc.h
+++
@@ -1,8 +0,0 @@
-/* The actual stack saving function, which just stores the stack,
- * this declared in an .asm file
- */
-#define EXTERNAL_ASM switch_x64_msvc.asm
-extern void *tealet_slp_switch(void *(*save_state)(void*, void*),
-                        void *(*restore_state)(void*, void*),
-                        void *extra);
-
